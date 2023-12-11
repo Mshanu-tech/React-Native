7e66fe9010f0ce6159ab7edfdf44afb3b7009b74
Mshanu-tech <ashanu2004@gmail.com>
2023-12-11 10:25:31 +0530

    commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>vknzxkjvcnKXVp!</Text>
+      <Text>sdfnksdnfj</Text>
       <StatusBar style="auto" />
     </View>
   );
